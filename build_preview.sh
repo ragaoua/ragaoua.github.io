35cb5456422b73d9b14f834315b1b55de714dde4
Reda AGAOUA <reda.agaoua@gmail.com>
2025-02-16 08:15:28 +0100

    preview scripts now handles restarting the jekyll container

--- build_preview.sh
+++ build_preview.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-podman run \
-  --tty \
-  --detach \
-  --rm \
-  --name jekyll \
-  -v "./jekyll:/var/jekyll" \
-  -p 4000:4000 \
-  mrxder/jekyll-docker-arm64:latest \
-  bash -c "bundle install && bundle exec jekyll serve --host=0.0.0.0"
+readonly jekyll_image="mrxder/jekyll-docker-arm64:latest"
+readonly container_name="jekyll-ragaoua-github-io"
+
+if podman container exists "$container_name" 2>&1 >/dev/null ; then
+  podman restart "$container_name"
+else
+  podman run \
+    --tty \
+    --detach \
+    --rm \
+    --name "$container_name" \
+    -v "./jekyll:/var/jekyll" \
+    -p 4000:4000 \
+    "$jekyll_image" \
+    bash -c "bundle install && bundle exec jekyll serve --host=0.0.0.0"
+fi
 
 echo "http://127.0.0.1:4000"
