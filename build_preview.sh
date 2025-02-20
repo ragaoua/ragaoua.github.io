811120624df905fe50851142b1d210d652071015
Reda AGAOUA <reda.agaoua@gmail.com>
2025-02-20 20:51:35 +0100

    add debug option to build_preview script

--- build_preview.sh
+++ build_preview.sh
@@ -2,27 +2,43 @@
 
 set -e
 
+detach_option="--detach"
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -d|--debug)
+      detach_option=""
+      shift
+      ;;
+    *)
+      echo "Unknown option $1" >&2
+      exit 2
+      ;;
+  esac
+done
+readonly detach_option
+
 readonly jekyll_image="mrxder/jekyll-docker-arm64:latest"
 readonly container_name="jekyll-ragaoua-github-io"
 
 if podman container exists "$container_name" 2>&1 >/dev/null ; then
-  podman restart "$container_name" >/dev/null
-else
-  readonly bundler_volume="jekyll-ragaoua-github-io-bundler"
-  podman volume create --ignore $bundler_volume >/dev/null
-
-  podman run \
-    --quiet \
-    --tty \
-    --detach \
-    --rm \
-    --name "$container_name" \
-    -v "./jekyll:/var/jekyll" \
-    -v "$bundler_volume:/usr/local/bundle" \
-    -p 4000:4000 \
-    "$jekyll_image" \
-    bash -c "bundle install && bundle exec jekyll serve --host=0.0.0.0"
+  podman stop "$container_name" >/dev/null
 fi
+
+readonly bundler_volume="jekyll-ragaoua-github-io-bundler"
+podman volume create --ignore $bundler_volume >/dev/null
+
+podman run \
+  --quiet \
+  --tty \
+  $detach_option \
+  --rm \
+  --name "$container_name" \
+  -v "./jekyll:/var/jekyll" \
+  -v "$bundler_volume:/usr/local/bundle" \
+  -p 4000:4000 \
+  "$jekyll_image" \
+  bash -c "bundle install && bundle exec jekyll serve --host=0.0.0.0"
+
 
 echo "Container running, run the following command to see if it's ready :"
 echo "podman logs -f $container_name"
